99d78f008f9aa7dabe2ccb79653172a4e5f7924d
Thijs van der Heijden <thijsvdh12@gmail.com>
2021-05-05 17:29:56 +0200

    :construction: dynamic meta data

--- lib/hergetto_web/views/layout_view.ex
+++ lib/hergetto_web/views/layout_view.ex
@@ -1,11 +1,49 @@
 defmodule HergettoWeb.LayoutView do
   use HergettoWeb, :view
 
-  def meta_tags(attrs_list) do
-    Enum.map(attrs_list, &meta_tag/1)
+  @doc """
+  Generates meta tags for the provided attributes.
+  If none are provided it will use the default attributes specified in `config.exs`
+
+  ## Parameters
+
+  - attrs_list: The list of attributes
+
+  To pass different attributes add them to the `assigns` of the `socket`/`conn`
+  Example: ```elixir
+  def mount(_params, _session, socket) do
+    meta_attrs = [
+      %{name: "title", content: "Welcome!"},
+    ]
+
+    {:ok, assign(socket, meta_attrs: meta_attrs)}
+  end
+  ```
+  """
+  def meta_tags(attrs_list \\ %{}) do
+    default_list = Application.get_env(:hergetto, HergettoWeb.Meta)
+
+    Enum.map(default_list, fn attr ->
+      case find_attr_index(attr, attrs_list) do
+        nil -> attr
+        id ->
+          Map.merge(attr, Enum.at(attrs_list, id))
+      end
+    end)
+    |> Enum.map(&meta_tag/1)
   end
 
-  def meta_tag(attrs) do
+  defp meta_tag(attrs) do
     tag(:meta, Enum.into(attrs, []))
+  end
+
+  defp find_attr_index(attr, search_list) do
+    Enum.find_index(search_list, fn search_attrs ->
+      cond do
+        Map.has_key?(search_attrs, :name) && search_attrs[:name] == attr[:name] -> true
+        Map.has_key?(search_attrs, :property) && search_attrs[:property] == attr[:property] -> true
+        true -> false
+      end
+    end)
   end
 end
