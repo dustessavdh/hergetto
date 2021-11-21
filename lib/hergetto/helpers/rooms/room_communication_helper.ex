a6a00a878613739156ddeaed9573fdb5cd7e8e65
JoostdeJager <46704554+JoostdeJager@users.noreply.github.com>
2021-11-21 17:44:15 +0100

    Added support for events

--- lib/hergetto/helpers/rooms/room_communication_helper.ex
+++ lib/hergetto/helpers/rooms/room_communication_helper.ex
@@ -1,34 +1,26 @@
 defmodule Hergetto.Rooms.RoomCommunicationHelper do
   @moduledoc """
-  This module handles the communication between the services and the live views.
+  This module provides helper functions for communication.
   """
 
-  alias Phoenix.PubSub
   alias Hergetto.Structs.RoomEvent
 
   @doc """
-  Subscribes to notifications from the specified room.
+  This function creates a `RoomEvent`.
+
+  Returns `%RoomEvent{}.`
 
   ## Examples
 
-      iex>RoomCommunicationHelper.subscribe("f2d97ea1-ddaf-4949-b1bc-63766ca8d52b")
-      :ok
-
+      iex> RoomCommunicationHelper.create_event("play", nil, "me")
+      %Hergetto.Structs.RoomEvent{
+        data: nil,
+        date: ~U[2021-11-21 16:37:21.864894Z],
+        event: "play",
+        sender: "me"
+      }
   """
-  def subscribe(room) do
-    PubSub.subscribe(Hergetto.PubSub, room)
-  end
-
-  @doc """
-  Broadcasts an event with data to the specified room.
-
-  ## Examples
-
-      iex> RoomCommunicationHelper.broadcast("f2d97ea1-ddaf-4949-b1bc-63766ca8d52b", :pause, %{}, "3f8e8ef9-fea8-42da-a37d-f5f2074077ef")
-      :ok
-
-  """
-  def broadcast(room, event, data, sender) do
-    PubSub.broadcast(Hergetto.PubSub, room, %RoomEvent{event: event, data: data, sender: sender})
+  def create_event(event, data, sender) do
+    %RoomEvent{event: event, data: data, sender: sender, date: DateTime.utc_now()}
   end
 end
--- lib/hergetto/structs/room_event.ex
+++ lib/hergetto/structs/room_event.ex
@@ -3,5 +3,5 @@ defmodule Hergetto.Structs.RoomEvent do
   This module represents an event that gets broadcast by the RoomService.
   """
 
-  defstruct [:event, :data, :sender]
+  defstruct [:event, :data, :sender, :date]
 end
