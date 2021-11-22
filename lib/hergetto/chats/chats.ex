ed4564d3e171dc273aaeac0d581e89e72d7b1e4e
JoostdeJager <46704554+JoostdeJager@users.noreply.github.com>
2021-11-22 23:46:08 +0100

    Added documentation

--- lib/hergetto/chats/chats.ex
+++ lib/hergetto/chats/chats.ex
@@ -4,10 +4,36 @@ defmodule Hergetto.Chats do
   alias Hergetto.Chats.ChatService
   alias Hergetto.Rooms
 
+  @doc """
+  This function creates a new chat_service.
+
+  Returns `{:ok, uuid}`. This `uuid` represents a chat_service.
+
+  ## Examples
+
+      iex> Chats.create(nil)
+      {:ok, "f2d97ea1-ddaf-4949-b1bc-63766ca8d52b"}
+
+  """
   def create(room) do
     ServiceStartHelper.start(ChatSupervisor, ChatService, room)
   end
 
+
+  @doc """
+  This function sends a message in the chat service.
+
+  Returns `:ok`.
+
+  ## Examples
+
+      iex> {:ok, chat} |>  Chats.create(nil)
+      {:ok, "f2d97ea1-ddaf-4949-b1bc-63766ca8d52b"}
+      iex> chat |> Chats.send(%Message{})
+      :ok
+
+
+  """
   def send(chat_service, message) do
     case chat_service |> exists() do
       true ->
@@ -22,6 +48,7 @@ defmodule Hergetto.Chats do
     end
   end
 
+  @doc false
   def get(chat_service, scope) do
     case chat_service |> exists() do
       true ->
@@ -32,26 +59,29 @@ defmodule Hergetto.Chats do
     end
   end
 
+  @doc false
   def get_all(chat_service) do
     chat_service |> get(:all)
   end
 
+  @doc false
   def get_messages(chat_service) do
     chat_service |> get(:messages)
   end
 
+  @doc false
   def get_room(chat_service) do
     chat_service |> get(:room)
   end
 
   @doc """
-  Check if the specified `room` exists.
+  Check if the specified `chat_service` exists.
 
   Returns `true` or `false`.
 
   ## Examples
 
-      iex> Rooms.exists("93a628cc-cec1-4733-b513-aff5824b02da")
+      iex> Chats.exists("93a628cc-cec1-4733-b513-aff5824b02da")
       true
 
   """
@@ -64,6 +94,7 @@ defmodule Hergetto.Chats do
     end
   end
 
+  @doc false
   defp generate_chat_service_id(chat_service) do
     :"chat_service:#{chat_service}"
   end
