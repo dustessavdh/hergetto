d19cd130324ebdb86857dc5a1e6718729755d53f
JoostdeJager <46704554+JoostdeJager@users.noreply.github.com>
2021-11-22 12:05:01 +0100

    Created a 404 page

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :hergetto, Hergetto.Repo,
 # with webpack to recompile .js and .css sources.
 config :hergetto, HergettoWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: [
--- lib/hergetto_web/views/error_view.ex
+++ lib/hergetto_web/views/error_view.ex
@@ -13,4 +13,8 @@ defmodule HergettoWeb.ErrorView do
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
+
+  def render("404.html", assigns) do
+    render("custom_404.html", conn: assigns.conn)
+  end
 end
