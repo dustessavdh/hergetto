0f5423b09301e499c263a9ae5c783099c3c6c87c
Thijs van der Heijden <thijsvdh12@gmail.com>
2021-12-30 23:20:48 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Hergetto.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.0"},
+      {:phoenix_html, "~> 3.2.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.16.4"},
       {:floki, ">= 0.30.0", only: :test},
