5ffb38952e2309dd3f04f720831034d3596bb1d3
Thijs van der Heijden <thijsvdh12@gmail.com>
2021-12-30 23:30:51 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Hergetto.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0"},
+      {:phoenix, "~> 1.6.5"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
