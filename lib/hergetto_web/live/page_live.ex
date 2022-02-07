0814b7dbda108414f690ee3bd03f68ebf48c3c78
Thijs van der Heijden <thijsvdh12@gmail.com>
2022-02-07 03:07:36 +0100

    Moves auth.helper.ex to different folder

--- lib/hergetto_web/live/page_live.ex
+++ lib/hergetto_web/live/page_live.ex
@@ -3,20 +3,28 @@ defmodule HergettoWeb.PageLive do
   use HergettoWeb, :live_view
   alias HergettoWeb.Components.Hero
   alias HergettoWeb.Components.LogoIcon
-  alias Hergetto.Helpers.AuthHelper
+  alias HergettoWeb.Helpers.UserLiveAuth
 
   @impl true
   def mount(_params, session, socket) do
     {
       :ok,
-      socket
-      |> fetch(session)
-      |> assign(room: %{:name => "test", :private? => true})
+      UserLiveAuth.fetch_current_user(socket, session)
     }
   end
 
-  def fetch(socket, session) do
-    socket
-    |> AuthHelper.assign_user(session)
-  end
+  # @impl true
+  # def mount(_params, session, socket) do
+  #   {
+  #     :ok,
+  #     socket
+  #     |> fetch(session)
+  #     |> assign(room: %{:name => "test", :private? => true})
+  #   }
+  # end
+
+  # def fetch(socket, session) do
+  #   socket
+  #   |> AuthHelper.assign_user(session)
+  # end
 end
