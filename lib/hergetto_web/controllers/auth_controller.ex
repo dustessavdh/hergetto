59251ffc13998889e68336d5d5f1bae1545ad7a8
Thijs van der Heijden <thijsvdh12@gmail.com>
2022-02-26 17:58:25 +0100

    Changed authentication flow

--- lib/hergetto_web/controllers/auth_controller.ex
+++ lib/hergetto_web/controllers/auth_controller.ex
@@ -3,13 +3,15 @@ defmodule HergettoWeb.AuthController do
   plug Ueberauth
   alias Hergetto.Accounts
   alias Hergetto.Accounts.User
+  alias Hergetto.Accounts.UserHelper, as: UH
   alias Hergetto.Authentication.Guardian
 
   def callback(%{assigns: %{ueberauth_auth: auth_info}} = conn, _params) do
     user_params = %{
-      profile_picture: auth_info.info.image,
+      profile_picture: Routes.static_path(conn, "/assets/avatars/default.svg"),
       external_id: auth_info.uid,
-      provider: Atom.to_string(auth_info.provider)
+      provider: Atom.to_string(auth_info.provider),
+      email: auth_info.info.email
     }
 
     login_or_register(conn, user_params)
@@ -39,18 +41,34 @@ defmodule HergettoWeb.AuthController do
     end
   end
 
-  defp login(conn, user) do
+  defp login(conn, user, redirect_url \\ "/") do
     conn
     |> put_flash(:info, "Signed in as #{user.username}")
     |> Guardian.Plug.sign_in(user)
     |> Guardian.Plug.remember_me(user)
-    |> redirect(to: "/")
+    |> redirect(to: redirect_url)
   end
 
   defp register(conn, user_params) do
-    {:ok, token, _claims} = Guardian.encode_and_sign(user_params)
-    conn
-    |> put_session(:user_to_register, token)
-    |> redirect(to: "/register")
+    case create_user(user_params) do
+      {:ok, user} ->
+        login(conn, user, "/account")
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error registering!")
+        |> redirect(to: "/login")
+    end
+  end
+
+  defp create_user(user_params) do
+    username = UH.generate_username()
+
+    user_params = user_params
+      |> Map.put_new(:username, username)
+      |> Map.put_new(:tag, UH.generate_tag())
+      |> Map.put_new(:profile_colour, UH.get_color_for_username(username))
+
+    Accounts.create_user(user_params)
   end
 end
