0e6dcba0c4f35dbac7c71efcb8489ba729fedb8d
Thijs van der Heijden <thijsvdh12@gmail.com>
2022-02-28 00:22:39 +0100

    Update postgres version in mix.exs

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule Hergetto.MixProject do
       {:phoenix, "~> 1.6.6"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, "~> 0.16.2"},
       {:phoenix_html, "~> 3.2.0"},
       {:phoenix_live_reload, "~> 1.3", only: :dev},
       {:phoenix_live_view, "~> 0.17.7"},
